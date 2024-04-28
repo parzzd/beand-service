f0bc5b5571de63b0a30d203d84fe47b0ff7cd31f
ChristianPue <christianaraujoc11@gmail.com>
2024-04-27 19:51:18 -0500

    US19
    Notificaciones de nuevas publicaciones en el foro de la comunidad

---
+++ beand-service/src/main/java/com/hampcode/bankingservice/model/entities/Article.java
@@ -0,0 +1,28 @@
+package com.hampcode.bankingservice.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "articles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "article_qualification", nullable = false)
+    private String articleQualification;
+
+    @Column(name = "article_description", nullable = false)
+    private String articleDescription;
+}
