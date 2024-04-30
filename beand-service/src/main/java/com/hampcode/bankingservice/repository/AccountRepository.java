35bc8feeff2c277710d369a57a014ae37653bc7d
TomasNM229 <67438238+TomasNM229@users.noreply.github.com>
2024-04-30 17:05:28 -0500

    Update AccountRepository.java

--- beand-service/src/main/java/com/hampcode/bankingservice/repository/AccountRepository.java
+++ beand-service/src/main/java/com/hampcode/bankingservice/repository/AccountRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.hampcode.bankingservice.model.entities.Account;
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
-    Optional<Account> findByAccountNumber(String accountNumber);
     Optional<Account> findByOwnerEmail(String ownerEmail);
 }
